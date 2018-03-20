c3f71f982661d9b6ee6147138fd2f2021a7ef4cc
Sho Uemura <meeg@slac.stanford.edu>
2018-03-20 14:26:28 -0600

    do both St1 and St2, add axis labels

--- dpalign/efficiency.py
+++ dpalign/efficiency.py
@@ -11,110 +11,156 @@ inFile = TFile("dpalign.root")
 #outFile = TFile(remainder[0]+".root","RECREATE")
 events = inFile.Get("save")
 
-outfilename="blah"
+outfilename="efficiency"
 
 c = TCanvas("c","c",1200,900);
 c.Print(outfilename+".pdf[")
 
 
-events.Draw("ty*797+y0:tx*797+x0>>hexp2d(200,-50,50,200,-50,50)","","colz");
-c.Print("blah.pdf");
 
-events.Draw("ty*797+y0:tx*797+x0>>hexp2d_withhit(200,-50,50,200,-50,50)","abs(fElementID-fElementID_exp+1)<2","colz");
-c.Print("blah.pdf");
+def make_effplots():
+    events.Draw("{0}:{1}>>hexp2d({2},{3})".format(y_var,x_var,x_binning,y_binning),"","colz");
+    c.Print(outfilename+".pdf");
 
-gDirectory.Get("hexp2d_withhit").Divide(gDirectory.Get("hexp2d"));
-gDirectory.Get("hexp2d_withhit").GetZaxis().SetRangeUser(0,1);
-gDirectory.Get("hexp2d_withhit").Draw("colz");
-c.Print("blah.pdf");
+    events.Draw("{0}:{1}>>hexp2d_withhit({2},{3})".format(y_var,x_var,x_binning,y_binning),hitcut,"colz");
+    c.Print(outfilename+".pdf");
+
+    numerator = gDirectory.Get("hexp2d_withhit")
+    denominator = gDirectory.Get("hexp2d")
+    #numerator.Sumw2();
+    numerator.Divide(denominator)
+    numerator.GetZaxis().SetRangeUser(0,1)
+    numerator.SetTitle("efficiency vs. position, {0}, no quad cut;extrapolated x [cm];extrapolated y [cm]".format(station))
+    numerator.Draw("colz");
+    c.Print(outfilename+".pdf");
+    for quad in range(0,4):
+        quadcut = "quad=={0}".format(quad)
+        flipped_x = x_var+"*({0})".format(1 if quad%2==0 else -1) # extrapolated X flipped such that the fiducial area is on the positive side
+        flipped_y = y_var+"*({0})".format(1 if quad<2 else -1) # extrapolated Y flipped such that the fiducial area is on the positive side
+        fiducialcut_x = "{0}>{1}".format(flipped_x,xedge[quad])
+        fiducialcuts = "{0} && {1}>{2}".format(fiducialcut_x,flipped_y,yedge[quad])
+
+        '''
+        events.Draw("fElementID_exp-1>>hhits_exp(40,-0.5,39.5)",quadcut,"");
+        events.Draw("fElementID_exp-1>>hhits(40,-0.5,39.5)",quadcut+" && "+hitcut,"");
+        gDirectory.Get("hhits").Sumw2();
+        gDirectory.Get("hhits").Divide(gDirectory.Get("hhits_exp"));
+        gDirectory.Get("hhits").Draw();
+        c.Print(outfilename+".pdf");
+        '''
+
+        events.Draw(bar_var+">>hhits_exp(60,-0.5,59.5)",quadcut+" && "+fiducialcuts,"");
+        #c.Print(outfilename+".pdf");
+        events.Draw(bar_var+">>hhits(60,-0.5,59.5)",quadcut+" && "+fiducialcuts+" && "+hitcut,"");
+        #c.Print(outfilename+".pdf");
+        numerator = gDirectory.Get("hhits")
+        denominator = gDirectory.Get("hhits_exp")
+        numerator.Sumw2();
+        numerator.Divide(denominator)
+        numerator.GetYaxis().SetRangeUser(0,1.1)
+        numerator.SetTitle("efficiency vs. bar, {0}, quad {1};bar ID;efficiency".format(station,quad))
+        numerator.Draw();
+        c.Print(outfilename+".pdf");
+
+        '''
+        leg = TLegend(0.0,0.75,0.2,0.9)
+        for xslice in range(0,10):
+            expslicehistname = "hhits_exp_{0}".format(xslice)
+            events.Draw("fElementID_exp>>{0}(40,-0.5,39.5)".format(expslicehistname),"quad=={0}&&abs(tx*797+x0-{1})<5".format(quad,xslice*10-45),"");
+            slicehistname = "hhits_{0}".format(xslice)
+            #print(slicehistname)
+            events.Draw("fElementID>>{0}(40,-0.5,39.5)".format(slicehistname),"quad=={0} && abs(fElementID-fElementID_exp+1)<2&&abs(tx*797+x0-{1})<5".format(quad,xslice*10-45),"");
+            #print("fElementID>>hhits(40,-0.5,39.5)","quad=={0} && abs(fElementID-fElementID_exp+1)<2&&abs(tx*797+x0-{1})<5".format(quad,xslice*10-45,slicehistname))
+            #print("fElementID>>{0}(40,-0.5,39.5)","quad=={0} && abs(fElementID-fElementID_exp+1)<2&&abs(tx*797+x0-{1})<5".format(quad,xslice*10-45,slicehistname))
+            #events.Draw("fElementID>>{2}(40,-0.5,39.5)","quad=={0} && abs(fElementID-fElementID_exp+1)<2&&abs(tx*797+x0-{1})<5".format(quad,xslice*10-45,slicehistname),"");
+            #gDirectory.ls()
+            hist = gDirectory.Get(slicehistname)
+            gDirectory.Get(slicehistname).Sumw2();
+            gDirectory.Get(slicehistname).Divide(gDirectory.Get(expslicehistname));
+            gDirectory.Get(slicehistname).Draw();
+            hist.SetLineColor(xslice+1)
+            leg.AddEntry(hist,"abs(tx*797+x0-{1})<5".format(quad,xslice*10-45))
+            if (xslice==0):
+                gDirectory.Get(slicehistname).GetYaxis().SetRangeUser(0,1);
+                hs = THStack("hs",gDirectory.Get(slicehistname).GetTitle());
+                gDirectory.Get(slicehistname).DrawClone();
+            else:
+                gDirectory.Get(slicehistname).DrawClone("same");
+            hs.Add(gDirectory.Get(slicehistname))
+            #c.Print(outfilename+".pdf");
+        hs.Draw("nostack")
+        leg.Draw()
+        c.Print(outfilename+".pdf");
+        '''
+
+        events.Draw(flipped_y+">>hy_exp({0})".format(flipped_y_binning),quadcut+" && "+fiducialcut_x,"");
+        #c.Print(outfilename+".pdf");
+        events.Draw(flipped_y+">>hy({0})".format(flipped_y_binning),quadcut+" && "+fiducialcut_x+" && "+hitcut,"");
+        #c.Print(outfilename+".pdf");
+        numerator = gDirectory.Get("hy")
+        denominator = gDirectory.Get("hy_exp")
+        #numerator.Sumw2();
+        numerator.Divide(denominator)
+        numerator.GetYaxis().SetRangeUser(0,1.1)
+        numerator.SetTitle("efficiency vs. position, {0}, quad {1};extrapolated y [cm];efficiency".format(station,quad))
+        numerator.Draw();
+        c.Print(outfilename+".pdf");
+
+        #events.Draw("ty*797+y0:tx*797+x0>>hexp2d_fid(200,-50,50,200,-50,50)",fiducialcuts,"colz");
+        #c.Print(outfilename+".pdf");
+
+        events.Draw("{0}:{1}>>hexp2d({2},{3})".format(y_var,x_var,x_binning,y_binning),quadcut,"colz");
+        c.Print(outfilename+".pdf");
+        events.Draw("{0}:{1}>>hexp2d_withhit({2},{3})".format(y_var,x_var,x_binning,y_binning),quadcut+" && "+hitcut,"colz");
+        c.Print(outfilename+".pdf");
+        numerator = gDirectory.Get("hexp2d_withhit")
+        denominator = gDirectory.Get("hexp2d")
+        #numerator.Sumw2();
+        numerator.Divide(denominator)
+        numerator.GetZaxis().SetRangeUser(0,1)
+        numerator.SetTitle("efficiency vs. position, {0}, quad {1};extrapolated x [cm];extrapolated y [cm]".format(station,quad))
+        numerator.Draw("colz");
+        c.Print(outfilename+".pdf");
+
+        '''
+        events.Draw("fElementID_exp-1:tx*797+x0>>hhits2d_exp(20,-50,50,40,-0.5,39.5)",quadcut,"");
+        events.Draw("fElementID_exp-1:tx*797+x0>>hhits2d(20,-50,50,40,-0.5,39.5)",quadcut+" && "+hitcut,"");
+        gDirectory.Get("hhits2d").Divide(gDirectory.Get("hhits2d_exp"));
+        gDirectory.Get("hhits2d").GetZaxis().SetRangeUser(0,1);
+        gDirectory.Get("hhits2d").Draw("colz");
+        c.Print(outfilename+".pdf");
+        '''
+
+station = "St1"
+x_binning = "200,-50,50"
+y_binning = "500,-50,50"
+flipped_y_binning = "500,0,50"
 
 yedge = [8.7, 8.3, 7.1, 7.4]
 xedge = [-2.0, 2.0, -2.0, 2.0]
 
-for quad in range(0,4):
-    quadcut = "quad=={0}".format(quad)
-    flipped_x = "(tx*797+x0)*({0})".format(1 if quad%2==0 else -1) # extrapolated X flipped such that the fiducial area is on the positive side
-    flipped_y = "(ty*797+y0)*({0})".format(1 if quad<2 else -1) # extrapolated Y flipped such that the fiducial area is on the positive side
-    fiducialcuts = "{0}>{1} && {2}>{3}".format(flipped_x,xedge[quad],flipped_y,yedge[quad])
-    hitcut = "abs(fElementID-fElementID_exp+1)<2" # require hit in the extrapolated bar or the adjacent bar
+x_var = "(tx*797+x0)"
+y_var = "(ty*797+y0)"
 
-    '''
-    events.Draw("fElementID_exp-1>>hhits_exp(40,-0.5,39.5)",quadcut,"");
-    events.Draw("fElementID_exp-1>>hhits(40,-0.5,39.5)",quadcut+" && "+hitcut,"");
-    gDirectory.Get("hhits").Sumw2();
-    gDirectory.Get("hhits").Divide(gDirectory.Get("hhits_exp"));
-    gDirectory.Get("hhits").Draw();
-    c.Print("blah.pdf");
-    '''
+bar_var = "fElementID_exp-1"
+hitcut = "abs(fElementID-fElementID_exp+1)<2" # require hit in the extrapolated bar or the adjacent bar
 
-    events.Draw("fElementID_exp-1>>hhits_exp(40,-0.5,39.5)",quadcut+" && "+fiducialcuts,"");
-    #c.Print("blah.pdf");
-    events.Draw("fElementID_exp-1>>hhits(40,-0.5,39.5)",quadcut+" && "+fiducialcuts+" && "+hitcut,"");
-    #c.Print("blah.pdf");
-    gDirectory.Get("hhits").Sumw2();
-    gDirectory.Get("hhits").Divide(gDirectory.Get("hhits_exp"));
-    gDirectory.Get("hhits").Draw();
-    c.Print("blah.pdf");
+make_effplots()
 
-    '''
-    leg = TLegend(0.0,0.75,0.2,0.9)
-    for xslice in range(0,10):
-        expslicehistname = "hhits_exp_{0}".format(xslice)
-        events.Draw("fElementID_exp>>{0}(40,-0.5,39.5)".format(expslicehistname),"quad=={0}&&abs(tx*797+x0-{1})<5".format(quad,xslice*10-45),"");
-        slicehistname = "hhits_{0}".format(xslice)
-        #print(slicehistname)
-        events.Draw("fElementID>>{0}(40,-0.5,39.5)".format(slicehistname),"quad=={0} && abs(fElementID-fElementID_exp+1)<2&&abs(tx*797+x0-{1})<5".format(quad,xslice*10-45),"");
-        #print("fElementID>>hhits(40,-0.5,39.5)","quad=={0} && abs(fElementID-fElementID_exp+1)<2&&abs(tx*797+x0-{1})<5".format(quad,xslice*10-45,slicehistname))
-        #print("fElementID>>{0}(40,-0.5,39.5)","quad=={0} && abs(fElementID-fElementID_exp+1)<2&&abs(tx*797+x0-{1})<5".format(quad,xslice*10-45,slicehistname))
-        #events.Draw("fElementID>>{2}(40,-0.5,39.5)","quad=={0} && abs(fElementID-fElementID_exp+1)<2&&abs(tx*797+x0-{1})<5".format(quad,xslice*10-45,slicehistname),"");
-        #gDirectory.ls()
-        hist = gDirectory.Get(slicehistname)
-        gDirectory.Get(slicehistname).Sumw2();
-        gDirectory.Get(slicehistname).Divide(gDirectory.Get(expslicehistname));
-        gDirectory.Get(slicehistname).Draw();
-        hist.SetLineColor(xslice+1)
-        leg.AddEntry(hist,"abs(tx*797+x0-{1})<5".format(quad,xslice*10-45))
-        if (xslice==0):
-            gDirectory.Get(slicehistname).GetYaxis().SetRangeUser(0,1);
-            hs = THStack("hs",gDirectory.Get(slicehistname).GetTitle());
-            gDirectory.Get(slicehistname).DrawClone();
-        else:
-            gDirectory.Get(slicehistname).DrawClone("same");
-        hs.Add(gDirectory.Get(slicehistname))
-        #c.Print("blah.pdf");
-    hs.Draw("nostack")
-    leg.Draw()
-    c.Print("blah.pdf");
-    '''
+station = "St2"
+x_binning = "200,-100,100"
+y_binning = "1200,-120,120"
+flipped_y_binning = "1200,0,120"
 
-    events.Draw(flipped_y+">>hy_exp(500,0,50)",quadcut+" && "+fiducialcuts,"");
-    #c.Print("blah.pdf");
-    events.Draw(flipped_y+">>hy(500,0,50)",quadcut+" && "+fiducialcuts+" && "+hitcut,"");
-    #c.Print("blah.pdf");
-    #gDirectory.Get("hy").Sumw2();
-    gDirectory.Get("hy").Divide(gDirectory.Get("hy_exp"));
-    gDirectory.Get("hy").Draw();
-    c.Print("blah.pdf");
+yedge = [8.2, 8.2, 7.5, 13.3]
+xedge = [1.0, -1.0, 1.0, -1.0]
 
-    #events.Draw("ty*797+y0:tx*797+x0>>hexp2d_fid(200,-50,50,200,-50,50)",fiducialcuts,"colz");
-    #c.Print("blah.pdf");
+x_var = "(tx*1497+x0)"
+y_var = "(ty*1497+y0)"
 
-    events.Draw("ty*797+y0:tx*797+x0>>hexp2d(200,-50,50,500,-50,50)",quadcut,"colz");
-    c.Print("blah.pdf");
-    events.Draw("ty*797+y0:tx*797+x0>>hexp2d_withhit(200,-50,50,500,-50,50)",quadcut+" && "+hitcut,"colz");
-    c.Print("blah.pdf");
-    gDirectory.Get("hexp2d_withhit").Divide(gDirectory.Get("hexp2d"));
-    gDirectory.Get("hexp2d_withhit").GetZaxis().SetRangeUser(0,1);
-    gDirectory.Get("hexp2d_withhit").Draw("colz");
-    c.Print("blah.pdf");
+bar_var = "bElementID_exp-1"
+hitcut = "abs(bElementID-bElementID_exp+1)<2" # require hit in the extrapolated bar or the adjacent bar
 
-    '''
-    events.Draw("fElementID_exp-1:tx*797+x0>>hhits2d_exp(20,-50,50,40,-0.5,39.5)",quadcut,"");
-    events.Draw("fElementID_exp-1:tx*797+x0>>hhits2d(20,-50,50,40,-0.5,39.5)",quadcut+" && "+hitcut,"");
-    gDirectory.Get("hhits2d").Divide(gDirectory.Get("hhits2d_exp"));
-    gDirectory.Get("hhits2d").GetZaxis().SetRangeUser(0,1);
-    gDirectory.Get("hhits2d").Draw("colz");
-    c.Print("blah.pdf");
-    '''
+make_effplots()
 
-c.Print("blah.pdf]");
+c.Print(outfilename+".pdf]");
